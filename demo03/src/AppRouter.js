163f73bef731d54447e204ee5079f69bb1c0946f
lixiaoyu <lixiaoyu@cont.com.cn>
2019-11-06 10:42:27 +0800

    1

--- demo03/src/AppRouter.js
+++ demo03/src/AppRouter.js
@@ -1,29 +1,41 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Index from "./page/Index";
-import './index.css'
+import Video from "./page/Video";
+import Work from "./page/Work";
+import "./index.css";
 
 function AppRouter() {
+  let routeConfig = [
+    { path: "/", title: "博客首页", exact: true, component: Index },
+    { path: "/video/", title: "视频教程", exact: false, component: Video },
+    { path: "/workplace/", title: "职场技能", exact: false, component: Work }
+  ];
+  const list = routeConfig.map((item, index) => {
+    return (
+      <li key={index}>
+        <Link to={item.path}>{item.title}</Link>
+      </li>
+    );
+  });
+  const route = routeConfig.map((item, index) => {
+    return (
+      <Route
+        key={index}
+        path={item.path}
+        exact={item.exact}
+        component={item.component}
+      />
+    );
+  });
   return (
     <Router>
       <div className="mainDiv">
         <div className="leftNav">
           <h3>一级导航</h3>
-          <ul>
-            <li>
-              <Link to="/">博客首页</Link>
-            </li>
-            <li>
-              <Link to="/">视频教程</Link>
-            </li>
-            <li>
-              <Link to="/">职场技能</Link>
-            </li>
-          </ul>
+          <ul>{list}</ul>
         </div>
-        <div className="rightMain">
-          <Route path="/" exact component={Index} />
-        </div>
+        <div className="rightMain">{route}</div>
       </div>
     </Router>
   );
