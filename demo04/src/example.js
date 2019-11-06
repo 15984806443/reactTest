940815f70a747faad58b591e23457ffd9eb627bb
lixiaoyu <lixiaoyu@cont.com.cn>
2019-11-06 17:52:41 +0800

    1

--- demo04/src/example.js
+++ demo04/src/example.js
@@ -2,12 +2,21 @@ import React, { useState } from "react";
 
 const Example = () => {
   const [count, setCount] = useState(0);
+  const [value, setValue] = useState("");
   return (
     <div>
       <p>You clicked {count} times</p>
+      <input
+        value={value}
+        onChange={e => {
+          setValue(e.target.value);
+        }}
+        type="text"
+      />
       <button
         onClick={() => {
           setCount(count + 1);
+          console.log(value);
         }}
       >
         Chlick me
--- demo04/src/index.js
+++ demo04/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./example9";
+import App from "./example";
 
 ReactDOM.render(<App />, document.getElementById("root"));
