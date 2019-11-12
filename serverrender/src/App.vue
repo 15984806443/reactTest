03eecb593c9a89a3f9c2222033fbf3029d59029f
lixiaoyu <lixiaoyu@cont.com.cn>
2019-11-12 18:02:58 +0800

    1

---
+++ serverrender/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Foo></Foo>
+    <Bar></Bar>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Foo from "./components/Foo.vue";
+import Bar from "./components/Bar.vue";
+
+@Component({
+  components: {
+    Foo,
+    Bar
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="stylus"></style>
